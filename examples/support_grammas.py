cd5d646557effb884b68afa1f1783022d3cd1b0d
Clay Chiang <chiangbing@gmail.com>
2012-11-12 00:59:06 +0800

    update README

--- examples/support_grammas.py
+++ examples/support_grammas.py
@@ -4,18 +4,18 @@
 
 #
 header_template = '''
-TYPE                               EXPRESSION                 OUPUT
------                              -----------                -----'''
+ TYPE                               EXPRESSION                 OUPUT
+ ----                               -----------                -----'''
 
 line_template = '''
-"Literal:                           "   "\\"0.1\\"                      " "0.1"                           "\n"  # This is another comment
-"Reference 1:                       "   "&uo                        " &uo                             "\n"
-"Builtin Gens:                      "   "${uo=url}                  " ${uo=url}                       "\n"
-"Reference 2:                       "   "&uo                        " &uo                             "\n"
-"Builtin Function 1:                "   "${=hex(3)}                 " ${=hex(3)}                      "\n"
-"Builtin Function 2:                "   "${=int(\\"01010\\", base=2)    " ${=int("01010", base=2)}        "\n"
-"Builtin Gens 2:                    "   "${=int}                    " ${=int}                         "\n"
-"Builtin Gens 3:                    "   "${=int(min=1, max=10)}     " ${=int(min=1, max=10)}          "\n"
+" Literal:                           "   "\\"0.1\\"                      " "0.1"                           "\n"  # This is another comment
+" Reference 1:                       "   "&uo                        " &uo                             "\n"
+" Builtin Gens:                      "   "${uo=url}                  " ${uo=url}                       "\n"
+" Reference 2:                       "   "&uo                        " &uo                             "\n"
+" Builtin Function 1:                "   "${=hex(3)}                 " ${=hex(3)}                      "\n"
+" Builtin Function 2:                "   "${=int(\\"01010\\", base=2)    " ${=int("01010", base=2)}        "\n"
+" Builtin Gens 2:                    "   "${=int}                    " ${=int}                         "\n"
+" Builtin Gens 3:                    "   "${=int(min=1, max=10)}     " ${=int(min=1, max=10)}          "\n"
 '''
 
 line_number = 3
